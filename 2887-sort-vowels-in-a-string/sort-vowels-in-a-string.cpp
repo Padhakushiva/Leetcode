c8ab931e73d7adc66ddbb380be6fba12326bf951
Shiva Choudhry <145893913+Padhakushiva@users.noreply.github.com>
2026-07-03 23:43:51 +0530

    Time: 14 ms (63.87%) | Memory: 15.6 MB (84.71%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    bool isVowel(char ch) {
-        ch = tolower(ch);
-        return ch == 'a' || ch == 'e' || ch == 'i' ||
-               ch == 'o' || ch == 'u';
+
+    bool isvowel(char c){
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-
     string sortVowels(string s) {
-        vector<char> vowels;
-
-        for (char ch : s) {
-            if (isVowel(ch)) {
-                vowels.push_back(ch);
+        vector<char>vowels;
+        for(char c:s){
+            if(isvowel(c)){
+                vowels.push_back(c);
             }
         }
-
         sort(vowels.begin(), vowels.end());
-
-        int j = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            if (isVowel(s[i])) {
-                s[i] = vowels[j++];
+        int j=0;
+        for(int i=0; i<s.size(); i++){
+            if(isvowel(s[i])){
+                s[i]=vowels[j++];
             }
         }
-
         return s;
     }
 };
